53133a3bb5e6083f985d66d8ac2668f2216493af
Peter <petr.ovchinnicov@gmail.com>
2024-06-16 18:06:47 +0300

    some code formatting

--- src/main/java/com/zer/morewaterlogging/mixin/special/CandleCakeBlockMixin.java
+++ src/main/java/com/zer/morewaterlogging/mixin/special/CandleCakeBlockMixin.java
@@ -3,19 +3,14 @@ package com.zer.morewaterlogging.mixin.special;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CandleCakeBlock;
 import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CandleCakeBlock.class)
 public abstract class CandleCakeBlockMixin {
-
-    @Shadow @Final public static BooleanProperty LIT;
 
     /**
      * @since 1.0.1
@@ -26,7 +21,7 @@ public abstract class CandleCakeBlockMixin {
         cir.setReturnValue(
             state.isIn(
                 BlockTags.CANDLE_CAKES, (statex) ->
-                    !state.get(LIT) && !state.get(Properties.WATERLOGGED)
+                    !state.get(Properties.LIT) && !state.get(Properties.WATERLOGGED)
             )
         );
     }
