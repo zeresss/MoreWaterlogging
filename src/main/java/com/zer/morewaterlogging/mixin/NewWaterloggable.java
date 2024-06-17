963cbdd377ccdde45661aa4701e96fb8f296f276
Peter <petr.ovchinnicov@gmail.com>
2024-06-17 14:50:18 +0300

    changed blocks waterlogging

--- src/main/java/com/zer/morewaterlogging/mixin/NewWaterloggable.java
+++ src/main/java/com/zer/morewaterlogging/mixin/NewWaterloggable.java
@@ -5,11 +5,10 @@ import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin({
     AbstractBannerBlock.class, AbstractButtonBlock.class, AbstractPressurePlateBlock.class, AbstractSkullBlock.class,
-    AnvilBlock.class, AzaleaBlock.class, BedBlock.class, BellBlock.class, BrewingStandBlock.class, CakeBlock.class,
-    CandleCakeBlock.class, CarpetBlock.class, ComparatorBlock.class, DaylightDetectorBlock.class, DoorBlock.class,
+    AnvilBlock.class, BeaconBlock.class, BedBlock.class, BellBlock.class, BrewingStandBlock.class, CakeBlock.class,
+    CandleCakeBlock.class, CarpetBlock.class, ComposterBlock.class, DaylightDetectorBlock.class, DoorBlock.class,
     DragonEggBlock.class, EnchantingTableBlock.class, EndPortalFrameBlock.class, EndRodBlock.class, FenceGateBlock.class,
-    FlowerPotBlock.class, GrindstoneBlock.class, HopperBlock.class, LecternBlock.class, LeverBlock.class,
-    PistonBlock.class, PistonExtensionBlock.class, PistonHeadBlock.class, RedstoneTorchBlock.class,
-    RedstoneWireBlock.class, RepeaterBlock.class, StonecutterBlock.class
+    GrindstoneBlock.class, HopperBlock.class, LeavesBlock.class, LecternBlock.class, LeverBlock.class, PistonBlock.class,
+    PistonExtensionBlock.class, PistonHeadBlock.class, ShulkerBoxBlock.class, StonecutterBlock.class, SpawnerBlock.class
 })
 public interface NewWaterloggable extends Waterloggable {}
