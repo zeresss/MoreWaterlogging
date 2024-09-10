ce99a1cda16951e699ff3b500933cdef493d0bc0
Peter <petr.ovchinnicov@gmail.com>
2024-09-10 17:13:13 +0300

    fixed door waterlogging bug

--- src/main/java/com/zer/morewaterlogging/mixin/special/DoorBlockMixin.java
+++ src/main/java/com/zer/morewaterlogging/mixin/special/DoorBlockMixin.java
@@ -7,14 +7,27 @@ import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(DoorBlock.class)
 public abstract class DoorBlockMixin {
+
+    /**
+     * @since 1.2.1
+     * makes door block correctly handle the flowing of water
+     * */
+    @Inject(method = "getStateForNeighborUpdate", at = @At("RETURN"), cancellable = true)
+    public void getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos, CallbackInfoReturnable<BlockState> cir) {
+        cir.setReturnValue(cir.getReturnValue().with(Properties.WATERLOGGED, world.getFluidState(pos).getFluid() == Fluids.WATER));
+    }
 
     /**
      * @since 1.0.0
