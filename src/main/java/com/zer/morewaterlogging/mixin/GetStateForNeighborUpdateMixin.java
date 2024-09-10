aa55d306201f2721312548b2d6668ddf5198ce5b
Peter <petr.ovchinnicov@gmail.com>
2024-09-10 18:01:32 +0300

    code formatting

--- src/main/java/com/zer/morewaterlogging/mixin/GetStateForNeighborUpdateMixin.java
+++ src/main/java/com/zer/morewaterlogging/mixin/GetStateForNeighborUpdateMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin({
+@Mixin(value = {
     AbstractPressurePlateBlock.class, BannerBlock.class, BedBlock.class, BellBlock.class, CakeBlock.class,
     CandleCakeBlock.class, CarpetBlock.class, DoorBlock.class, FenceGateBlock.class, PistonHeadBlock.class, WallBannerBlock.class
 })
@@ -21,7 +21,7 @@ public abstract class GetStateForNeighborUpdateMixin {
      * @since 1.0.0
      * makes block correctly handle the flowing of water
      */
-    @Inject(method = "getStateForNeighborUpdate", at = @At("HEAD"))
+    @Inject(method = "getStateForNeighborUpdate", at = @At(value = "HEAD"))
     private void getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos, CallbackInfoReturnable<BlockState> cir) {
         if (this instanceof NewWaterloggable && state.contains(Properties.WATERLOGGED) && state.get(Properties.WATERLOGGED)) {
             world.getFluidTickScheduler().schedule(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
